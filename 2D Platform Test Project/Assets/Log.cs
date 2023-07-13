3f7ff7aa613bd0cfb5e0ef66f66076261d53283d
BaxterJack <baxterjack@hotmail.co.uk>
2023-07-13 16:48:09 +0100

    Implementing pickup logs

--- 2D Platform Test Project/Assets/Log.cs
+++ 2D Platform Test Project/Assets/Log.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Log : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    TMP_Text pickupDialogue;
+    string pickUp = "Press E to pick up Log";
+    public float textYOffset = 0.5f;
+    public TMP_Text textPrefab;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            CreatePickupText();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Pickup log");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DestroyPickupText();
+        }
+    }
+    void CreatePickupText()
+    {
+        pickupDialogue = Instantiate(textPrefab);
+        pickupDialogue.text = pickUp;
+        pickupDialogue.gameObject.transform.position = GetDialogueTextPosition();
+    }
+
+    void DestroyPickupText()
+    {
+        if (pickupDialogue != null)
+        {
+            Destroy(pickupDialogue.gameObject);
+        }
+    }
+
+
+
+    Vector3 GetDialogueTextPosition()
+    {
+        Vector3 npcPosition = this.transform.position;
+        npcPosition.y += textYOffset;
+        return npcPosition;
     }
 }
